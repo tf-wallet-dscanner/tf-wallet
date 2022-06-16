549b533ccad2a549fbe933a423d2bedc2f740e99
jtpark <jtpark@deleo.co.kr>
2022-06-16 16:50:55 +0900

    refactor: 빌드 산출물 관련 소스 정리
    기존 webpack 설정에는 public/index.html, index.js 를 바라보고 있어서 필요없는 파일들임에도 불구하고 빌드시 필요했었음.
    - popup에 대한 소스를 pages/*.ts|tsx 로 지정
    - 필요없어진 popup/*.tsx 등 파일 삭제
    - webpack에서 빌드 경로 및 변수명 수정
    - 필수 파일 예외처리 부분에서 popup을 바라보도록 수정
    
    close #refactor/directories

--- src/ui/pages/index.tsx
+++ src/ui/pages/index.tsx
@@ -4,20 +4,20 @@ import 'ui/styles/global.scss';
 import App from './App';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('popup') as HTMLElement,
 );
 const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
